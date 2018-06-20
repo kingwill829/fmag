cf937c75dc1a5de68cb8cb44c99d190bd46eb7c4
Will King <willking829@gmail.com>
2018-06-20 17:02:29 +0100

    finished javascript

--- product-page/scripts.js
+++ product-page/scripts.js
@@ -1,39 +1,34 @@
 $(document).ready(function(){
 
 	$('.product-info__item-header, .product-info__desktop-nav-item').click(function() {
-		var productInfoItem = "#" + $(this).attr('rel');
+		
+		var productInfoItem = $(this).attr('rel');
+		var productInfoItemID = "#" + $(this).attr('rel');
 
-		var productInfoItemDetail = productInfoItem + " .product-info__item-detail";
-
-		var productInfoItemIcon = productInfoItem + " .product-info__item-icon";
-
+		// check if clicked item is active
 		if ( $(this).parent().hasClass('product-info__item--active')  ) {
+			// if it is active then remove active class to hide it
 			$(this).parent().removeClass('product-info__item--active')
 		} 
-			else {
-					$('.product-info__item').removeClass('product-info__item--active');
-					$(productInfoItem).addClass('product-info__item--active');
+		else {
+				// hide all other items
+				$('.product-info__item').removeClass('product-info__item--active');
+				// make the clicked item active
+				$(productInfoItemID).addClass('product-info__item--active');
 
-					// Add active class to desktop prod desc menu items on click
-					if ($(this).hasClass('product-info__desktop-nav-item') ) {
-						$('.product-info__desktop-nav-item').removeClass('product-info__desktop-nav-item--active');
-						$(this).addClass('product-info__desktop-nav-item--active');			
-						
-					}
-			};
+				// Add active class to desktop prod desc menu items on click
+			  $('.product-info__desktop-nav-item').removeClass('product-info__desktop-nav-item--active');
+				$('.product-info__desktop-nav-item[rel="' + productInfoItem + '"]').addClass('product-info__desktop-nav-item--active');
+
+			} // end else
+			
 		}); // end click function
 		
 	}); // end document ready
 
 $(window).resize(function (){
 	if ( $(window).width() > 767 ) {
-		$('.product-info__item-detail').removeAttr('style');
-		
-		if (!$('.product-info__item').hasClass('product-info__item--active') ) {
-			$('#product-description').addClass('product-info__item--active');		
-				
-		}
-
-	} 
-
+			var productInfoItemDesktopNav = $('.product-info__desktop-nav-item--active').attr('rel');
+			$('#' + productInfoItemDesktopNav).addClass('product-info__item--active');
+		} 
 });
